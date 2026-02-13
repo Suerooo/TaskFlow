c81b05de9de78a8189c449160062d9864834c517
Suerooo <raulsuerodaw@gmail.com>
2026-02-13 10:32:10 +0000

    Add toString and equals/hashCode

--- src/model/User.java
+++ src/model/User.java
@@ -61,4 +61,50 @@ public class User {
         this.hashPassword = BCrypt.hashpw(password, BCrypt.gensalt(6));
     }
 
+    @Override
+    public String toString() {
+        return String.format("ID: %d%n Username: %s%n Email: %s%n Hashpassword: %s%n",
+                this.ID, this.username, this.email, this.hashPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ID;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((hashPassword == null) ? 0 : hashPassword.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (ID != other.ID)
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (hashPassword == null) {
+            if (other.hashPassword != null)
+                return false;
+        } else if (!hashPassword.equals(other.hashPassword))
+            return false;
+        return true;
+    }
+
 }
